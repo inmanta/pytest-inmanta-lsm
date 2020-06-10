d54aeb035da4c572c6dad80c4d108cbe02479319
arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
2020-06-10 13:34:26 +0200

    Fix bug where --lsm_noclean defaults to True (#3)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-inmanta-lsm",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">=3.6",  # also update classifiers
     author="Inmanta",
     author_email="code@inmanta.com",
