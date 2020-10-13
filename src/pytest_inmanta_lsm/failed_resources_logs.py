0db2405a311d3b54837b9c012147d173a5af9dcb
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2020-10-13 16:59:16 +0200

    Checking get_version_result code when extracting logs

--- src/pytest_inmanta_lsm/failed_resources_logs.py
+++ src/pytest_inmanta_lsm/failed_resources_logs.py
@@ -31,32 +31,35 @@ class FailedResourcesLogs:
         """
         logs = []
 
-        if get_version_result.code == 200:
-            for resource in get_version_result["resources"]:
-                resource_id = resource["resource_id"]
+        for resource in get_version_result["resources"]:
+            resource_id = resource["resource_id"]
 
-                # Only interested in failed resources
-                if resource["status"] != "failed":
+            # Only interested in failed resources
+            if resource["status"] != "failed":
+                continue
+
+            for action in resource["actions"]:
+                if "messages" not in action:
                     continue
 
-                for action in resource["actions"]:
-                    if "messages" not in action:
-                        continue
-
-                    logs.extend([(message, resource_id) for message in action["messages"]])
-        else:
-            LOGGER.warn(
-                f"Couldn't get error logs, got response code {get_version_result.code} (expected 200): \n{get_version_result}"
-            )
+                logs.extend([(message, resource_id) for message in action["messages"]])
 
         return logs
 
     def _retrieve_logs(self) -> List[Tuple[str, str]]:
-        get_version_result = self._client.get_version(
-            tid=self._environment_id, id=self._find_version(), include_logs=True
-        ).get_result()
+        version = self._find_version()
+        if version is None:
+            return []
 
-        return self._extract_logs(get_version_result)
+        get_version_result = self._client.get_version(tid=self._environment_id, id=version, include_logs=True).get_result()
+
+        if get_version_result.code == 200:
+            return self._extract_logs(get_version_result)
+        else:
+            LOGGER.warn(
+                f"Couldn't get error logs, got response code {get_version_result.code} (expected 200): \n{get_version_result}"
+            )
+            return []
 
     def _find_version(self) -> int:
         versions = self._client.list_versions(tid=self._environment_id).result["versions"]
@@ -64,7 +67,7 @@ class FailedResourcesLogs:
         # assumption - version with highest number will be the latest one
         if len(versions) == 0:
             LOGGER.warn(f"No versions provided for environment {self._environment_id}, picking version 0")
-            return 0
+            return None
 
         return max(version_item["version"] for version_item in versions)
 
