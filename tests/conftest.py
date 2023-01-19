0443fdd213b3dafaf85bf35325573ef2c45fc8a9
Sander Van Balen <git@sandervanbalen.be>
2023-01-19 14:47:15 +0100

    backported cleanup fixtures from pytest-inmanta

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@
 
 import os
 import sys
+from typing import Optional
 
 import pkg_resources
 import pytest
@@ -25,12 +26,20 @@ def set_cwd(testdir):
 
 
 @pytest.fixture(scope="function", autouse=True)
+def reset_pytest_inmanta_state():
+    yield
+    pytest_inmanta.plugin.ProjectLoader.reset()
+
+
 def deactive_venv():
     old_os_path = os.environ.get("PATH", "")
     old_prefix = sys.prefix
     old_path = sys.path
     old_meta_path = sys.meta_path.copy()
     old_path_hooks = sys.path_hooks.copy()
+    old_pythonpath = os.environ.get("PYTHONPATH", None)
+    old_os_venv: Optional[str] = os.environ.get("VIRTUAL_ENV", None)
+    old_working_set = pkg_resources.working_set
 
     yield
 
@@ -44,7 +53,20 @@ def deactive_venv():
     sys.path_hooks.extend(old_path_hooks)
     # Clear cache for sys.path_hooks
     sys.path_importer_cache.clear()
-    pkg_resources.working_set = pkg_resources.WorkingSet._build_master()
+    # Restore PYTHONPATH
+    if old_pythonpath is not None:
+        os.environ["PYTHONPATH"] = old_pythonpath
+    elif "PYTHONPATH" in os.environ:
+        del os.environ["PYTHONPATH"]
+    # Restore VIRTUAL_ENV
+    if old_os_venv is not None:
+        os.environ["VIRTUAL_ENV"] = old_os_venv
+    elif "VIRTUAL_ENV" in os.environ:
+        del os.environ["VIRTUAL_ENV"]
     # stay compatible with older versions of core: don't call the function if it doesn't exist
+    if hasattr(env, "mock_process_env"):
+        env.mock_process_env(python_path=sys.executable)
     if hasattr(PluginModuleFinder, "reset"):
         PluginModuleFinder.reset()
+    plugins.PluginMeta.clear()
+    loader.unload_inmanta_plugins()
