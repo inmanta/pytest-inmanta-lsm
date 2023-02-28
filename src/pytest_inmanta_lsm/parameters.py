77cf5e4f35ce3e41d3451c52ac5a8e9998c8970c
Marcin Chron <marcin.chron@amartus.com>
2023-02-28 15:02:05 +0100

    Update help message of new cli parameters

--- src/pytest_inmanta_lsm/parameters.py
+++ src/pytest_inmanta_lsm/parameters.py
@@ -105,7 +105,7 @@ inm_lsm_env = StringTestParameter(
 inm_lsm_project_name = StringTestParameter(
     argument="--lsm-project-name",
     environment_variable="INMANTA_LSM_PROJECT_NAME",
-    usage="Project name to be used for this environment",
+    usage="Project name to be used for this environment. Used only when new environment is created, otherwise this parameter is ignored",
     default="pytest-inmanta-lsm",
     group=param_group,
 )
@@ -113,7 +113,7 @@ inm_lsm_project_name = StringTestParameter(
 inm_lsm_env_name = StringTestParameter(
     argument="--lsm-environment-name",
     environment_variable="INMANTA_LSM_ENVIRONMENT_NAME",
-    usage="Environment name",
+    usage="Environment name. Used only when new environment is created, otherwise this parameter is ignored",
     default="pytest-inmanta-lsm",
     group=param_group,
 )
