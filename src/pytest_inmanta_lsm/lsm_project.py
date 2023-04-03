a94f6c00c6726d9e9ec623570ccfa8f39f012a27
João Trindade <joao.pedro.trindade@inmanta.com>
2023-04-03 10:02:01 +0100

    import from inmanta_lsm directly

--- src/pytest_inmanta_lsm/lsm_project.py
+++ src/pytest_inmanta_lsm/lsm_project.py
@@ -18,7 +18,7 @@ import inmanta_lsm.const
 import inmanta_lsm.model
 import pytest
 import pytest_inmanta.plugin
-from inmanta_lsm.dict_path import DictPath
+from inmanta_lsm.dict_path import DictPath, to_path
 
 # Error message to display when the lsm module is not reachable
 INMANTA_LSM_MODULE_NOT_LOADED = (
@@ -219,10 +219,8 @@ class LsmProject:
         # Edit logic derived from:
         # https://github.com/inmanta/inmanta-lsm/blob/39e9319381ce6cfc9fd22549e2b5a9cc7128ded2/src/inmanta_lsm/model.py#L2794
 
-        from inmanta_plugins.lsm.allocation_v2 import dict_path
-
         for current_edit in edit:
-            dict_path_obj: DictPath = dict_path.to_path(current_edit.target)
+            dict_path_obj: DictPath = to_path(current_edit.target)
 
             if current_edit.operation == inmanta_lsm.model.EditOperation.replace.value:
                 dict_path_obj.set_element(service.candidate_attributes, current_edit.value)
