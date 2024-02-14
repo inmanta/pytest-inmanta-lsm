298b4c0f47f117eb316afba693894e71f05bc5ff
hugo <hugo.thiolliere@inmanta.com>
2024-02-14 17:38:18 +0100

    []

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 setup(
     name="pytest-inmanta-lsm",
-    version="0.0.3",
+    version="3.2.0",
     python_requires=">=3.6",  # also update classifiers
     author="Inmanta",
     author_email="code@inmanta.com",
