4d66635d8838eceae56a55d551a5acc55ca38bc0
Hugo Lloreda <hugo.lloreda.sanchez@inmanta.com>
2024-04-15 16:55:55 +0200

    refactor

--- src/pytest_inmanta_lsm/load_generator.py
+++ src/pytest_inmanta_lsm/load_generator.py
@@ -6,17 +6,13 @@
     :license: Inmanta EULA
 """
 
+import asyncio
 import datetime
-import functools
 import logging
 import threading
 import time
-from dataclasses import dataclass
 
-import inmanta_plugins.restbase.utils
-import requests
-from inmanta_plugins.restbase import handler_logger
-from requests_toolbelt.utils import dump
+from pytest_inmanta_lsm import remote_orchestrator
 
 LOGGER = logging.getLogger(__name__)
 
@@ -52,125 +48,38 @@ class LoadThread(threading.Thread):
 
         We override it to raise any exception that the Thread could have faced
         """
-        threading.Thread.join(self)
-        # Since join() returns in caller thread
+        super().join(timeout)
         # we re-raise the caught exception
         # if any was caught
         if self.exc:
             raise self.exc
 
 
-@dataclass
-class LoadTask:
-    description: str
-    urls: list[str]
-
-
-def create_list_of_tasks(environment: str, service_type: str) -> list[LoadTask]:
-    """
-    Create and return a list of pre-defined tasks
-
-    :param environment: The environment to use
-    :param service_type: The type of Service that will exist on the environment
-    """
-    current_datetime = datetime.datetime.utcnow()
-    current_formatted_datetime = current_datetime.isoformat()[:-3] + "Z"
-    to_formatted_datetime = (current_datetime + datetime.timedelta(days=7)).isoformat()[:-3] + "Z"
-
-    tasks = [
-        LoadTask(
-            description="Background load calls",
-            urls=[
-                "/api/v2/notification?limit=100&filter.cleared=false",
-                f"/api/v2/environment/{environment}?details=false",
-            ],
-        ),
-        LoadTask(
-            description="Metrics page call",
-            urls=[
-                "/api/v2/metrics?metrics=lsm.service_count&metrics=lsm.service_instance_count&metrics=orchestrator."
-                "compile_time&metrics=orchestrator.compile_waiting_time&metrics=orchestrator.compile_rate&"
-                "metrics=resource.agent_count&metrics=resource.resource_count&start_interval="
-                f"{current_formatted_datetime}&end_interval={to_formatted_datetime}&nb_datapoints=15&"
-                "round_timestamps=true"
-            ],
-        ),
-        LoadTask(description="Service catalog overview call", urls=["/lsm/v1/service_catalog?instance_summary=True"]),
-        LoadTask(
-            description="Catalog for a specific service type calls",
-            urls=[
-                f"/lsm/v1/service_catalog/{service_type}?instance_summary=True",
-                f"/lsm/v1/service_inventory/{service_type}?include_deployment_progress=True&limit=20&&sort=created_at.desc",
-            ],
-        ),
-        LoadTask(description="Compile reports call", urls=["/api/v2/compilereport?limit=20&sort=requested.desc"]),
-        LoadTask(
-            description="Resources view call",
-            urls=["/api/v2/resource?deploy_summary=True&limit=20&filter.status=%21orphaned&sort=resource_type.asc"],
-        ),
-    ]
-    return tasks
-
-
 class LoadGenerator:
     def __init__(
         self,
-        base_url: str,
-        environment: str,
+        remote_orchestrator: remote_orchestrator.RemoteOrchestrator,
         service_type: str,
+        logger: logging.Logger = LOGGER,
         sleep_time: float = 1.0,
     ):
-        self.base_url = base_url
+        self.remote_orchestrator = remote_orchestrator
+        self.service_type = service_type
         self.sleep_time = sleep_time
-
-        self.tasks = create_list_of_tasks(environment, service_type)
-        self.logger = handler_logger.LoggerWrapper(LOGGER)
-        self._session = self.init_session(environment)
+        self.logger = logger
         self._lock = threading.Lock()
         self.SHOULD_THREAD_RUN = True
-        self._thread = threading.Thread(target=self.create_load, daemon=True, name="Thread-LG")
+        self._thread = LoadThread(target=self.between_callback, daemon=True, name="Thread-LG")
 
     def __enter__(self) -> None:
-        self.logger.debug("Starting Thread %(thread)s", thread=self._thread.name)
+        self.logger.debug("Starting Thread %s", self._thread.name)
         self._thread.start()
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.change_status()
-        self.logger.debug("Stopping Thread %(thread)s", thread=self._thread.name)
+        self.logger.debug("Stopping Thread %s", self._thread.name)
         self._thread.join(30)
-        self.logger.debug("Thread %(thread)s has been stopped, closing session", thread=self._thread.name)
-        self._session.close()
-        self.logger.debug("Session has been closed")
-
-    def init_session(self, environment: str) -> requests.Session:
-        """
-        Method that is making sure that a new session is created and that every request will be logged and have a timeout on it
-        """
-        session = requests.Session()
-
-        # Make sure that we log all requests
-        session.request = functools.partial(
-            inmanta_plugins.restbase.utils.logged_request,
-            session.request,
-            self.logger,
-        )
-
-        session.request = functools.partial(
-            inmanta_plugins.restbase.utils.prefixed_request,
-            session.request,
-            self.base_url,
-        )
-
-        # Add a default timeout of 10 seconds to all requests made
-        session.request = functools.partial(
-            inmanta_plugins.restbase.utils.timeout_request,
-            session.request,
-            10,
-        )
-
-        session.headers.update({"X-Inmanta-tid": environment})
-
-        return session
+        self.logger.debug("Thread %s has been stopped, closing session", self._thread.name)
 
     def fetch_thread_status(self) -> bool:
         """
@@ -179,69 +88,112 @@ class LoadGenerator:
         with self._lock:
             return self.SHOULD_THREAD_RUN
 
-    def log_response(self, response: requests.Response):
-        """
-        Log the received response. If we are not able to dump the response, we will only log the method and the URL
-
-        :param response: The response that we need to log
-        """
-        try:
-            response.raise_for_status()
-            self.logger.debug(
-                "Thread %(thread)s - %(response)s", thread=self._thread.name, response=dump.dump_all(response).decode("utf-8")
-            )
-        except requests.RequestException:
-            try:
-                self.logger.error(
-                    "Thread %(thread)s - %(response)s",
-                    thread=self._thread.name,
-                    response=dump.dump_all(response).decode("utf-8"),
-                )
-            except UnicodeDecodeError:
-                self.logger.error(
-                    "Thread %(thread)s - Request not logged because of bad format: %(method)s %(url)s",
-                    thread=self._thread.name,
-                    method=response.request.method.upper(),
-                    url=response.url,
-                )
-        except UnicodeDecodeError:
-            self.logger.debug(
-                "Thread %(thread)s - Request not logged because of bad format: %(method)s %(url)s",
-                thread=self._thread.name,
-                method=response.request.method.upper(),
-                url=response.url,
-            )
-
-    def run_task(self, task: LoadTask) -> None:
-        """
-        Run the given task
-
-        :param task: The task that needs to be executed. A task can be composed of multiple URLs
-        """
-        self.logger.debug(task.description)
-
-        for url in task.urls:
-            response = self._session.get(url)
-            self.log_response(response)
-
-        time.sleep(self.sleep_time)
-
-    def create_load(self):
-        """
-        Loop to run provided tasks. This loop will only stop when `SHOULD_THREAD_RUN` is set to False
-        """
-        while True:
-            for task in self.tasks:
-                self.run_task(task)
-
-                should_run = self.fetch_thread_status()
-                if not should_run:
-                    return
-
     def change_status(self):
         """
         Change the `SHOULD_THREAD_RUN` status
         """
         with self._lock:
             self.SHOULD_THREAD_RUN = False
-        self.logger.debug("Thread %(thread)s should stop", thread=self._thread.name)
+        self.logger.debug("Thread %s should stop", self._thread.name)
+
+    def between_callback(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        loop.run_until_complete(self.create_load())
+        loop.close()
+
+    async def create_load(self):
+        """
+        Loop to run provided tasks. This loop will only stop when `SHOULD_THREAD_RUN` is set to False
+        """
+        start_datetime = datetime.datetime.utcnow()
+        nb_datapoints = 15
+        # start_interval and end_interval should be at least <nb_datapoints> minutes separated from each other
+        # But we need also to respect the following constraint:
+        # When round_timestamps is set to True, the number of hours between start_interval and end_interval should be
+        # at least the amount of hours equal to nb_datapoints
+        end_datetime = start_datetime + datetime.timedelta(hours=nb_datapoints + 1)
+
+        while True:
+            self.logger.debug("Thread %s - Background load calls", self._thread.name)
+            await self.remote_orchestrator.request(
+                "list_notifications",
+                tid=self.remote_orchestrator.environment,
+                limit=100,
+                filter={"cleared": False},
+            )
+            await self.remote_orchestrator.request(
+                "environment_get",
+                id=self.remote_orchestrator.environment,
+                details=False,
+            )
+
+            self.logger.debug("Thread %s - Metrics page call", self._thread.name)
+            await self.remote_orchestrator.request(
+                "get_environment_metrics",
+                tid=self.remote_orchestrator.environment,
+                metrics=[
+                    "lsm.service_count",
+                    "lsm.service_instance_count",
+                    "orchestrator.compile_time",
+                    "orchestrator.compile_waiting_time",
+                    "orchestrator.compile_rate",
+                    "resource.agent_count",
+                    "resource.resource_count",
+                ],
+                start_interval=start_datetime,
+                end_interval=end_datetime,
+                nb_datapoints=nb_datapoints,
+                round_timestamps=True,
+            )
+
+            self.logger.debug("Thread %s - Service catalog overview call", self._thread.name)
+            await self.remote_orchestrator.request(
+                "lsm_service_catalog_list",
+                tid=self.remote_orchestrator.environment,
+                instance_summary=True,
+            )
+
+            self.logger.debug("Thread %s - Catalog for a specific service type calls", self._thread.name)
+            try:
+                await self.remote_orchestrator.request(
+                    "lsm_service_catalog_get_entity",
+                    tid=self.remote_orchestrator.environment,
+                    service_entity=self.service_type,
+                    instance_summary=True,
+                )
+                await self.remote_orchestrator.request(
+                    "lsm_services_list",
+                    tid=self.remote_orchestrator.environment,
+                    service_entity=self.service_type,
+                    include_deployment_progress=True,
+                    limit=20,
+                    sort="created_at.desc",
+                )
+            except AssertionError:
+                self.logger.warning("Thread %s - Service entity `%s` not found!", self._thread.name, self.service_type)
+
+            self.logger.debug("Thread %s - Compile reports call", self._thread.name)
+            await self.remote_orchestrator.request(
+                "get_compile_reports",
+                tid=self.remote_orchestrator.environment,
+                limit=20,
+                sort="requested.desc",
+            )
+
+            self.logger.debug("Thread %s - Resources view call", self._thread.name)
+            await self.remote_orchestrator.request(
+                "resource_list",
+                tid=self.remote_orchestrator.environment,
+                deploy_summary=True,
+                limit=20,
+                filter={"status": "orphaned"},
+                sort="resource_type.asc",
+            )
+
+            should_run = self.fetch_thread_status()
+            if not should_run:
+                break
+
+            time.sleep(self.sleep_time)
