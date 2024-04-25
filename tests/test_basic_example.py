17d31c045fc7bee2cc726c92acab1ec3303a450e
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2024-04-25 13:47:35 +0200

    Fix regex

--- tests/test_basic_example.py
+++ tests/test_basic_example.py
@@ -26,10 +26,12 @@ def test_deployment_failure(testdir: pytest.Testdir):
     result.assert_outcomes(passed=1, failed=1)
 
     # Check that the dump has been created
-    search_line = re.compile(r"(.*)INFO(.*)Support archive of orchestrator has been saved at (.*)")
+    search_line = re.compile(
+        r"INFO\s+pytest_inmanta_lsm\.plugin:plugin\.py:\d+\s+Support archive of orchestrator has been saved at (?P<path>.*)"
+    )
     matched_lines = [match for line in result.stdout.lines if (match := search_line.fullmatch(line)) is not None]
     assert len(matched_lines) == 1, f"Failed to find dump log in test output: {result.stdout.str()}"
-    assert pathlib.Path(matched_lines[0].group(1)).exists()
+    assert pathlib.Path(matched_lines[0].group("path")).exists()
 
 
 def test_basic_example(testdir):
