e6ab0f879197441185c76e9a4947033f358c6301
hugo <hugo.thiolliere@inmanta.com>
2024-10-23 17:06:36 +0200

    wip

--- src/pytest_inmanta_lsm/remote_orchestrator.py
+++ src/pytest_inmanta_lsm/remote_orchestrator.py
@@ -262,11 +262,20 @@ class RemoteOrchestrator:
         # Save the version of the remote orchestrator server
         self._server_version: typing.Optional[Version] = None
 
+        use_old_disk_layout: bool = self.run_command(['if [-f "/var/lib/inmanta/.inmanta_use_new_disk_layout"]; then echo "True"; fi', ], user=None, stderr=subprocess.PIPE).strip() == "True"
+
         # The path on the remote orchestrator where the project will be synced
-        self.remote_project_path = pathlib.Path(
-            "/var/lib/inmanta/server/environments/",
-            str(self.environment),
-        )
+        if use_old_disk_layout:
+            self.remote_project_path = pathlib.Path(
+                "/var/lib/inmanta/server/environments/",
+                str(self.environment),
+            )
+        else:
+            self.remote_project_path = pathlib.Path(
+                "/var/lib/inmanta/server/",
+                str(self.environment),
+                "/compiler"
+            )
         self.remote_project_cache_path = self.remote_project_path.with_name(self.remote_project_path.name + "_cache")
 
     @property
