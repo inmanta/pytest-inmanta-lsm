834d21e1099ba61dc1f2b1090ba3fa3d21fa113e
João Trindade <joao.pedro.trindade@inmanta.com>
2025-11-06 13:02:19 +0000

    Discover postgres version based on compatibility.json in container (#536)
    * Discover postgres version based on compatibility.json in container
    
    * added mor documentation
    
    * fixed command
    
    * updated readme and documentation
    
    * bump to major version
    
    * Update CHANGELOG.md
    
    Co-authored-by: Sander Van Balen <git@sandervanbalen.be>
    
    ---------
    
    Co-authored-by: Sander Van Balen <git@sandervanbalen.be>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 setup(
     name="pytest-inmanta-lsm",
-    version="3.15.0",
+    version="4.0.0",
     python_requires=">=3.11",  # also update classifiers
     author="Inmanta",
     author_email="code@inmanta.com",
