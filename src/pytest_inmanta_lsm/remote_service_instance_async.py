5b1acb05f6e68ae7c482b6ce1154b39c083aad78
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2026-02-03 09:07:40 +0100

    Revert "update to newest jenkinsfile (#551)"
    This reverts commit a31a267ab81922b8d3345c239cfa8704ac714829.

--- src/pytest_inmanta_lsm/remote_service_instance_async.py
+++ src/pytest_inmanta_lsm/remote_service_instance_async.py
@@ -52,10 +52,7 @@ def get_service_instance_from_log(log: model.ServiceInstanceLog) -> model.Servic
             deployment_progress=None,
             service_identity_attribute_value=log.service_identity_attribute_value,
             referenced_by=None,
-        )  # type: ignore[call-arg]
-        # The model.ServiceInstance used in older versions of inmanta-lsm (iso7) had fewer fields than more recent versions,
-        # which means that we would have different mypy results for different supported inmanta-lsm versions.
-        # We add this ignore in order to have a consistent mypy-baseline between supported iso versions
+        )
 
 
 class RemoteServiceInstanceError(RuntimeError, typing.Generic[T]):
